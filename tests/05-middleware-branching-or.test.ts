bc6fa5fd5b0ba5276d6a1a43f01031f4d84bc19a
Tomas Savigliano <ralusek@gmail.com>
2020-05-03 14:24:10 -0700

    1.0.0

--- tests/05-middleware-branching-or.test.ts
+++ tests/05-middleware-branching-or.test.ts
@@ -1,4 +1,3 @@
-
 import 'mocha';
 import { expect } from 'chai';
 
@@ -7,21 +6,20 @@ import { Microservie } from '../lib/types';
 
 import microservie from '../lib/index';
 
-
 let instance: Microservie;
 
 describe('Middleware: Branching $or', () => {
   it('should accept branching $or', () => {
-    instance = microservie({
-      name: 'Branching $or'
-    }, () => [
+    instance = microservie(
       {
-        $or: [
-          () => {},
-          () => {}
-        ]
-      }
-    ]);
+        name: 'Branching $or',
+      },
+      () => [
+        {
+          $or: [() => {}, () => {}],
+        },
+      ]
+    );
   });
 
   it('should call all middleware simultaneously and resolve when the first resolves.', () => {
@@ -29,46 +27,48 @@ describe('Middleware: Branching $or', () => {
     const resolved: { [key in string]: boolean } = {};
     const errored: { [key in string]: Error } = {};
 
-    instance = microservie({
-      name: 'Branching $or'
-    }, () => [
+    instance = microservie(
       {
-        $or: [
-          () => {
-            called.a = true;
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                expect(errored.c).to.not.be.undefined;
-                expect(called.b).to.be.true;
-                expect(called.c).to.be.true;
-                expect(resolved.b).to.not.be.true;
-                resolve(resolved.a = true);
-              }, 15);
-            });
-          },
-          () => {
-            called.b = true;
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                expect(errored.c).to.not.be.undefined;
-                expect(called.a).to.be.true;
-                expect(called.c).to.be.true;
-                expect(resolved.a).to.be.true;
-                resolve(resolved.b = true);
-              }, 30);
-            });
-          },
-          () => {
-            called.c = true;
-            errored.c = new Error('Hey');
-            throw errored.c;
-          },
-        ]
-      }
-    ]);
+        name: 'Branching $or',
+      },
+      () => [
+        {
+          $or: [
+            () => {
+              called.a = true;
+              return new Promise((resolve) => {
+                setTimeout(() => {
+                  expect(errored.c).to.not.be.undefined;
+                  expect(called.b).to.be.true;
+                  expect(called.c).to.be.true;
+                  expect(resolved.b).to.not.be.true;
+                  resolve((resolved.a = true));
+                }, 15);
+              });
+            },
+            () => {
+              called.b = true;
+              return new Promise((resolve) => {
+                setTimeout(() => {
+                  expect(errored.c).to.not.be.undefined;
+                  expect(called.a).to.be.true;
+                  expect(called.c).to.be.true;
+                  expect(resolved.a).to.be.true;
+                  resolve((resolved.b = true));
+                }, 30);
+              });
+            },
+            () => {
+              called.c = true;
+              errored.c = new Error('Hey');
+              throw errored.c;
+            },
+          ],
+        },
+      ]
+    );
 
-    return instance.run({})
-    .then(() => {
+    return instance.run({}).then(() => {
       // All 3 were called.
       expect(called.a).to.be.true;
       expect(called.b).to.be.true;
